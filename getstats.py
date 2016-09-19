4e91e185ed522547ccfc2c3d21c1f19f42b9590d
kestasli <k.liaugminas@gmail.com>
2016-09-19 22:20:51 +0300

    More changes

--- getstats.py
+++ getstats.py
@@ -1,7 +1,20 @@
 import pandas as pd
 from sys import argv
+import os
+
+def getLogFiles(path):
+
+    filelist = os.listdir(path)
+    logfiles = []
+
+    for file in filelist:
+        if file.startswith('account-stats'):
+            logfiles.append(path + "/" + file)
+    return logfiles
 
 program, datafile = argv
+
+print getLogFiles('csv')
 
 df = pd.read_csv(datafile, sep = ', ', engine = 'python', index_col = False, skip_blank_lines = True)
 #print df
