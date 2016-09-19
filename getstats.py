8b328e70138513d0b524c7114620dcc156ec9934
kestasli <k.liaugminas@gmail.com>
2016-09-19 22:31:19 +0300

    Testsdasd

--- getstats.py
+++ getstats.py
@@ -17,7 +17,7 @@ program, datafile = argv
 print getLogFiles('csv')
 
 df = pd.read_csv(datafile, sep = ', ', engine = 'python', index_col = False, skip_blank_lines = True)
-#print df
+print df
 
 print df.groupby(['User']).mean().nlargest(50, ['Gigabytes'])
 print '\n'
