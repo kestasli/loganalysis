0c675be82ab4c5656826ef1c06a8f37312d44d5e
kestasli <k.liaugminas@gmail.com>
2016-12-01 13:56:13 +0200

    Merge branch 'master' of https://github.com/kestasli/loganalysis
    # Conflicts:
    #	whatever_with_css.html

--- bbstats_2.3.py
+++ bbstats_2.3.py
@@ -193,7 +193,7 @@ print 'Unique IP: ', len(uniqueip['IP'])
 print("Exec time: %s seconds" % (time.time() - start_time))
 
 if statsconfig.get('export', 'toexcel') == '1':
-    filtered.to_excel(statsconfig.get('export', 'excelname'), 'raw')
+    filtered.to_excel(statsconfig.get('export', 'excelname'), 'raw', columns=['DATETIME', 'CLIENT', 'URL'], index = False,)
 
 if statsconfig.get('export', 'tohtml') == '1':
     htmltable = group_client_url.to_html(columns=['CLIENT', 'URL', 'HITS'], index = False, justify = 'left')
